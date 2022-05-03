e6fd7911dd5bb318d687332e93f7cc297dadec1e
Sasha Dimbrovskyi <sanya.dimbrovskyi@gmail.com>
2022-05-03 11:20:56 +0300

    Update settings.py

--- backend/travelagency/travelagency/settings.py
+++ backend/travelagency/travelagency/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ul5x!+g)(0xsu5uv=+=gvc(gbjezgy6ekh(89kbf-emymz5=0w'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
