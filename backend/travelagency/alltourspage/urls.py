7cc39d976f83fe155ce417c699d998495ce9f908
sanarkk <sanya.dimbrovskyi@gmail.com>
2022-05-08 17:57:38 +0300

    Create personal page for tour

--- backend/travelagency/alltourspage/urls.py
+++ backend/travelagency/alltourspage/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import alltours_view
+from .views import alltours_view, showtour_view
 
 urlpatterns = [
-    path('', alltours_view, name="alltours")
+    path('', alltours_view, name="alltours"),
+    path('tour/<int:id>', showtour_view, name="tourshow")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/travelagency/alltourspage/views.py
+++ backend/travelagency/alltourspage/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from mainpage.models import TourModel
+from mainpage.models import TourModel, AdditionalInfoModel, DepartureInfoModel
 
 # Create your views here.
 def alltours_view(request):
     tours = TourModel.objects.all()
-    context = {'tours': tours}
+    context = {"tours": tours}
     return render(request, "tours/pages/alltours_page.html", context)
+
+
+def showtour_view(request, id):
+    tours = TourModel.objects.get(id=id)
+    addinfo = AdditionalInfoModel.objects.get(id=id)
+    departures = DepartureInfoModel.objects.get(id=id)
+    context = {"tours": tours, "addinfo": addinfo, "departures": departures}
+    return render(request, "tours/pages/tourpage.html", context)
